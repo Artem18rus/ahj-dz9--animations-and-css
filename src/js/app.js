b549f7e8c77e8e69a84d8deb326dd6359639f5ff
Artem18rus <urart88@mail.ru>
2022-12-11 16:53:00 +0400

    8

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,17 @@
+/* eslint-disable no-return-assign */
 const box = document.querySelector('.box');
 const button = document.querySelector('.button');
+const blockAnimation = document.querySelector('.block-animation');
 const displayAnimNone = document.querySelector('.displayAnimNone');
 
 button.addEventListener('click', () => {
   button.classList.toggle('bdr');
-  displayAnimNone.classList.toggle('displayAnimNone');
-  displayAnimNone.classList.toggle('block-animation');
+
+  blockAnimation.classList.toggle('displayAnimNone');
+  if (blockAnimation.classList.contains('displayAnimNone')) {
+    setTimeout(() => displayAnimNone.style.border = 'none', 800);
+  } else {
+    displayAnimNone.style.border = '1px solid #E0E0E0';
+  }
   box.style.border = '2px dashed #ffffff00';
 });
